4709e6f0b71fab6cfb38d7329d6a40feb8a8f818
John Kearon <jkearon@hotmail.com>
2025-04-21 10:20:50 -0400

    Swift lesson 61 in progress

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum Dice: Int, CaseIterable {  // CaseInterable to enable iterationg through the Dice enum
+    // CaseInterable to enable iterationg through the Dice enum
+    // Identifiable A class of types whose instances hold the value of an entity with stable identity
+    
+    enum Dice: Int, CaseIterable, Identifiable {
         case four = 4
         case six = 6
         case eight = 8
@@ -17,14 +20,23 @@ struct ContentView: View {
         case twenty = 20
         case hundred = 100
         
+        // Computed properties
+        var id: Int {   // make the enum Identifiable
+            rawValue
+        }
+        
+        var description: String { "\(rawValue)-sided" }
+        
         func roll() -> Int {
             return Int.random(in: 1...self.rawValue)
         }
     }
     
     @State private var resultMessage = ""
-
-
+    @State private var animationTrigger = false  // changes to this variable trigger the animation
+    @State private var isDoneAnimating = true
+    
+    
     var body: some View {
         VStack {
             Text("Dungeon Dice")
@@ -38,21 +50,38 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.medium)
                 .multilineTextAlignment(.center)
-                .frame(height: 159)
-            
+//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)  // animate to 1.0 font scale
+//                .opacity(isDoneAnimating ? 1.0 : 0.2)  // aniamate to 1.0 opacity
+//                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: 1.0  ).ro
+//                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1.0, y: 0.0, z: 0.0
+                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (x: 1.0, y: 0.0, z: 0.0))
+                .frame(height: 150)
+                .onChange(of: animationTrigger) {
+                    isDoneAnimating = false
+//                    withAnimation(.spring(Spring(response: 0.3, dampingRatio: 0.4))) {
+//                        isDoneAnimating = true
+//                    }
+                    withAnimation() {
+                        isDoneAnimating = true
+                    }
+
+                    }
+                }
+
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
-                ForEach(Dice.allCases, id: \.self) { die in
-                    Button("\(die.rawValue)-sided") {
+                ForEach(Dice.allCases) { die in
+                    Button(die.description) {
                         resultMessage = "You rolled a \(die.roll()) on a \(die.rawValue) - sided die."
+                        animationTrigger.toggle() // a change of this variable triggers an animation
                     }
                 }
             }
             .buttonStyle(.borderedProminent)
             .tint(.red)
-
-            }
+            
+        }
         .padding()
     }
 }
